4b5dd0ba49b9f5e1348351cd4cdef3aa968ca342
Gilbert <nicg771@gmail.com>
2026-06-06 20:25:07 +0700

    refactor(Service): fix Lazy Class and Dead Code

--- src/com/Hotel/Reservation/Service.java
+++ src/com/Hotel/Reservation/Service.java
@@ -1,30 +1,41 @@
 package com.Hotel.Reservation;
 
-abstract public class Service {
-	
-	int type;
-	int cost;
-	int quantity;
-	String bno;
-	boolean status;
-	
-	Service()
-	{
-		type=0;
-		cost=0;
-		quantity=0;
-		bno=null;
-		status=false;
-	}
-	abstract public void setDetails();
-	abstract public int getTotalCost();
-	abstract public boolean getStatus();
-	
-//	Service(int a,int b,int c,String d)
-//	{
-//		type=a;
-//		cost=b;
-//		quantity=c;
-//		bno=d;
-//	}
+public abstract class Service {
+
+    protected static final int COST_TYPE_1 = 100;
+    protected static final int COST_TYPE_2 = 200;
+    protected static final int COST_TYPE_3 = 300;
+
+    protected int type;
+    protected int cost;
+    protected int quantity;
+    protected String bno;
+    protected boolean status;
+
+    protected Service() {
+        type = 0;
+        cost = 0;
+        quantity = 0;
+        bno = null;
+        status = false;
+    }
+
+    public abstract void setDetails();
+
+    public int getTotalCost() {
+        return quantity * cost;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    protected int costForType(int type) {
+        switch (type) {
+            case 1: return COST_TYPE_1;
+            case 2: return COST_TYPE_2;
+            case 3: return COST_TYPE_3;
+            default: return 0;
+        }
+    }
 }

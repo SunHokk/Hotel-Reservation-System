135658a05175fc3c5102a778ef2806a0a3a920da
Gilbert <nicg771@gmail.com>
2026-06-06 20:24:27 +0700

    refactor(Room): fix Lazy Class by pulling up methods from all subclasses

--- src/com/Hotel/Reservation/Room.java
+++ src/com/Hotel/Reservation/Room.java
@@ -1,10 +1,30 @@
 package com.Hotel.Reservation;
 
-abstract class Room {
+public abstract class Room {
 
-	int rate;
-	boolean wifi;
-	boolean status;
-    
-    abstract public void statuschange();
+    protected int rate;
+    protected boolean wifi;
+    protected boolean status;
+
+    public void set(int rate, boolean wifi, boolean status) {
+        this.rate = rate;
+        this.wifi = wifi;
+        this.status = status;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public boolean getWifi() {
+        return wifi;
+    }
+
+    public void statuschange() {
+        status = !status;
+    }
 }
